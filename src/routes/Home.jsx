285086feae233be9408c8013b9850e4c73a13746
Robin Perricone <robinp31@uw.edu>
2024-03-02 21:29:30 -0700

    working state

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,33 @@
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import { fireAuth as auth,logout,isLoggedIn } from '../services/firebase/firebase';
+//'../../services/firebase/firebase.js';
+
+
 export function Home() {
     const [pokemonList, SetPokemonList] = useState([]);
     const [option, SetOption] = useState({next: '', previous: ''});
-
+    const navigate = useNavigate();
+    function loggedIn(){
+    onAuthStateChanged(auth, user => {
+        if (user) {
+            console.log(
+                'logged in!!!!'
+            )
+            return true
+        }
+        else {
+            console.log(
+                'not logged in!!!!'
+            )
+            navigate('/login')
+           return false
+        } })
+    }
+    loggedIn();
     useEffect(() => {
+      
         fetchPokemon();
     }
     , []);
@@ -34,6 +57,7 @@ export function Home() {
   return (
     <div>
       <h1>Pokedex</h1>
+        <button onClick={()=>{logout()}}>Logout</button>
       {pokemonList}
       {option.previous && <button onClick={()=>{fetchPokemon(option.next)}}> Previous </button>}
      <button onClick={()=>{fetchPokemon(option.next)}} > Next </button>
