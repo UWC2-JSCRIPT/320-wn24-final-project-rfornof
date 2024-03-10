1f3c20cdb43c044421625eb67acd729bc955dd25
Robin <robinp31@uw.edu>
2024-03-09 21:16:04 -0700

    book update works , next book review

--- src/routes/BookReview/BookReviewInsert.jsx
+++ src/routes/BookReview/BookReviewInsert.jsx
@@ -1,77 +1,164 @@
-import { db,fireAuth as auth,logout,isLoggedIn } from '../../services/firebase/firebase';
+import { db, fireAuth as auth, logout, isLoggedIn } from '../../services/firebase/firebase';
 import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { doc, setDoc } from "firebase/firestore";
 import './bookreview.css'
- import bookSvg from '../../assets/book.svg'
+import bookSvg from '../../assets/book.svg'
+import bookmarkSvg from '../../assets/bookmark1.svg'
+import { useNavigate } from 'react-router-dom';
 export function BookReviewInsert() {
-  isLoggedIn(auth);
-  const [bookNumber, setBookNumber] = useState(localStorage.getItem('book_number') || '');
-  const [bookType, setBookType] = useState(localStorage.getItem('book_type') || '');
-  const [title, setTitle] = useState(localStorage.getItem('title') || '');
-  const [author, setAuthor] = useState(localStorage.getItem('author') || '');
-  const [startDate, setStartDate] = useState(localStorage.getItem('start_date') || '');
-  const [finishDate, setFinishDate] = useState(localStorage.getItem('finish_date') || '');
-  const [rating, setRating] = useState(localStorage.getItem('rating') || '');
-  const [recommendedBy, setRecommendedBy] = useState(localStorage.getItem('recommended_by') || '');
-  const [pageLength, setPageLength] = useState(localStorage.getItem('page_length') || '');
-  const [isFiction, setIsFiction] = useState(JSON.parse(localStorage.getItem('is_fiction')) || false);
-  const [enjoyedEnding, setEnjoyedEnding] = useState(JSON.parse(localStorage.getItem('enjoyed_ending')) || false);
-  const [favoriteChar, setFavoriteChar] = useState(localStorage.getItem('favorate_char') || '');
-  const [readingEase, setReadingEase] = useState(localStorage.getItem('reading_ease') || '');
-  const [plot, setPlot] = useState(localStorage.getItem('plot') || '');
-  const [favoriteQuotes, setFavoriteQuotes] = useState(localStorage.getItem('favorite_quotes') || '');
-  const [reviewNotes, setReviewNotes] = useState(localStorage.getItem('review_notes') || '');
-  const [askAuthor, setAskAuthor] = useState(localStorage.getItem('ask_author') || '');
-  const [isRecommended, setIsRecommended] = useState(JSON.parse(localStorage.getItem('is_recommended')) || false);
+    const navigate = useNavigate();
+    isLoggedIn(auth);
+    const [bookNumber, setBookNumber] = useState(localStorage.getItem('book_number') || '');
+    const [bookType, setBookType] = useState(localStorage.getItem('book_type') || '');
+    const [title, setTitle] = useState(localStorage.getItem('title') || '');
+    const [author, setAuthor] = useState(localStorage.getItem('author') || '');
+    const [startDate, setStartDate] = useState( '');
+    const [finishDate, setFinishDate] = useState( '');
+    const [rating, setRating] = useState(localStorage.getItem('rating') || '');
+    // const [recommendedBy, setRecommendedBy] = useState(localStorage.getItem('recommended_by') || '');
+    // const [pageLength, setPageLength] = useState(localStorage.getItem('page_length') || '');
+    // const [isFiction, setIsFiction] = useState(JSON.parse(localStorage.getItem('is_fiction')) || false);
+    // const [enjoyedEnding, setEnjoyedEnding] = useState(JSON.parse(localStorage.getItem('enjoyed_ending')) || false);
+    // const [favoriteChar, setFavoriteChar] = useState(localStorage.getItem('favorate_char') || '');
+    // const [readingEase, setReadingEase] = useState(localStorage.getItem('reading_ease') || '');
+    // const [plot, setPlot] = useState(localStorage.getItem('plot') || '');
+    // const [favoriteQuotes, setFavoriteQuotes] = useState(localStorage.getItem('favorite_quotes') || '');
+    // const [reviewNotes, setReviewNotes] = useState(localStorage.getItem('review_notes') || '');
+    // const [askAuthor, setAskAuthor] = useState(localStorage.getItem('ask_author') || '');
+    // const [isRecommended, setIsRecommended] = useState(JSON.parse(localStorage.getItem('is_recommended')) || false);
+    const [stars,setStars] = useState([])
 
-  const handleSubmit = (e) => {
-      e.preventDefault();
 
-      const bookData = {
-          book_number: bookNumber,
-          book_type: bookType,
-          title: title,
-          author: author,
-          start_date: startDate,
-          finish_date: finishDate,
-          rating: rating,
-          recommended_by: recommendedBy,
-          page_length: pageLength,
-          is_fiction: isFiction,
-          enjoyed_ending: enjoyedEnding,
-          favorate_char: favoriteChar,
-          reading_ease: readingEase,
-          plot: plot,
-          favorite_quotes: favoriteQuotes,
-          review_notes: reviewNotes,
-          ask_author: askAuthor,
-          is_recommended: isRecommended
-      };
-      for (let key in bookData) {
-        localStorage.setItem(key, bookData[key]);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const bookData = {
+            book_number: bookNumber,
+            book_type: bookType,
+            title: title,
+            author: author,
+            start_date: startDate,
+            finish_date: finishDate,
+            rating: rating,
+            // recommended_by: recommendedBy,
+            // page_length: pageLength,
+            // is_fiction: isFiction,
+            // enjoyed_ending: enjoyedEnding,
+            // favorate_char: favoriteChar,
+            // reading_ease: readingEase,
+            // plot: plot,
+            // favorite_quotes: favoriteQuotes,
+            // review_notes: reviewNotes,
+            // ask_author: askAuthor,
+            // is_recommended: isRecommended
+        };
+        for (let key in bookData) {
+            localStorage.setItem(key, bookData[key]);
+        }
+        const uuid = uuidv4()
+        setDoc(doc(db, "review", uuid), bookData).then((doc) => {
+            console.log('sent document to review', uuid)
+            navigate(`/book_review_view/${uuid}`)
+        })
+    };
+    function star(stars = 0) {
+        setRating(stars)
+        const resultOut = []
+        for (let i = 0; i < 5; i++){
+            if(stars > i){
+                resultOut.push(<span onClick={()=>{
+                star(i+1)
+               
+                }} class="fa fa-star checked"></span>)
+                
+            }
+            else{
+                resultOut.push(<span onClick={()=>{
+                    star(i+1)
+                    }}  class="fa fa-star"></span>)
+            }
+
+           
+        }
+       setStars (resultOut)
+       
     }
-    const uuid = uuidv4()
-     setDoc(doc(db, "review", uuid), bookData).then((doc)=>{
-      console.log('sent document to review', uuid)
-     })
-  };
+    function selectBookType() {
+        const listOfRadioItems = [
+            ["ebook", "eBook"],
+            ["audiobook", "Audiobook"],
+            ["hardback", "HardBack"],
+            ["paperback", "PaperBack"],
+        ]
+        const resultOut = []
+        for (const item of listOfRadioItems) {
+            resultOut.push(<div className="type-grid-item"><input type="radio" id={item[0]} name="book_type" value={item[0]} onChange={() => {
+                setBookType(item[0])
+                console.log(item[0])}} />
+                <label for={item[0]}>{item[1]}</label><br /> </div>)
 
-  useEffect(()=>{
 
-    
-  },[])
-  return (
-    <>
-        <img src={bookSvg} className="book-background"></img>
+        }
+        return resultOut
+    }
+    useEffect(() => {
+star()
 
-    <div className="body-grid">
-        <br/>
-    <form onSubmit={handleSubmit} className="review-insert">
-      <div>
-        Book You Have Read
-      </div>
-            <div className='body-grid-item'>
+    }, [])
+    return (
+        <>
+       
+
+            <div className="type-grid">
+                {selectBookType()}
+            </div>
+            <div className="header-book">
+                <img src={bookSvg} className="book-background header-book-item"></img>
+                <span className="fonty header-book-item header-book-title">Book Review</span>
+                <div className="header-book-item header-book-bookmark">
+                    <img width="100%" src={bookmarkSvg} />
+                    <input type="text" onChange={(event) => { setBookNumber(event.target.value) }} className="header-book-item book-number"/>
+                </div>
+
+                <div className="header-book-item line-title " >
+                    Title
+                </div>
+                <div className="line line-title"></div>
+                <input type="text" className="line line-title  underneath-offset" onChange={(event) => { setTitle(event.target.value) }}/>
+                  
+                <div className="line line-author "> </div>
+
+                <div className="line-author"  >
+                    Author
+                </div>
+                <input type="text"  onChange={(event) => { setAuthor(event.target.value) }} 
+                className="line-author underneath-offset-author"/>
+                <div className='date-start'>
+                    <div className="date-label">Start-date <span className="space"></span>Finish-date<br/></div>
+                    <input type="date" value={startDate} onChange={e => setStartDate(e.target.value)} required />
+                    <div className="date-end">
+                        <input type="date" value={finishDate} onChange={e => setFinishDate(e.target.value)} required />
+                    </div>
+                    
+                </div>
+                
+            </div>
+            <div className="rating-grid">
+                <div className="rating-item">
+                Rating: {stars}
+                </div>
+         
+            </div>
+            <button type="submit" className="submit-button" onClick={(e)=>{handleSubmit(e)}}>Submit</button>
+
+            {/* <div className="body-grid"> */}
+
+            {/* <form onSubmit={handleSubmit} className="review-insert"> */}
+            {/* <div>
+                        
+                    </div> */}
+            {/* <div className='body-grid-item'>
                 <div className='book-type-grid'>
             <label className='book-type-grid-item'>
                 Book Number:
@@ -116,9 +203,9 @@ export function BookReviewInsert() {
         </label>
         <br/>
         </form>
-        </div> 
-        {/* grid end>  */}
-        <div className="grid-bottom">
+        </div>  */}
+            {/* grid end>  */}
+            {/* <div className="grid-bottom">
             <form>
         <label>
             Page Length:
@@ -172,7 +259,7 @@ export function BookReviewInsert() {
         <br/>
         <button type="submit">Submit</button>
     </form>
-    </div>
-    </>
-);
+    </div> */}
+        </>
+    );
 }
